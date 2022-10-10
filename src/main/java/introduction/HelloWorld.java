4b1fcd928118bb706d2e0b1cc54a13aa54e73964
s452737 <lukbie2@st.amu.edu.pl>
2022-10-10 16:43:19 +0200

    commit1

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,15 @@
 package introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
     public static void main(String [ ] args) {
+
+        Logger log = Logger.getLogger("name");
+
         System.out.println("Hello! Witaj!");
+
+        log.info("message");
     }
 }
